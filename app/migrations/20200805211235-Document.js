fc20b2b8ec925fa9c08f47c8d4c9f3c66dd8c2e3
dkingstong <d.kingston12@gmail.com>
2020-08-05 22:47:00 -0500

    All (#1)

---
+++ app/migrations/20200805211235-Document.js
@@ -0,0 +1,61 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('document', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.BIGINT,
+      },
+      document_id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        required: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      owner_name: {
+        type: Sequelize.STRING,
+      },
+      url: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      category: {
+        type: Sequelize.ENUM,
+        values: ['economics', 'political', 'social'],
+      },
+      version: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        required: true,
+      },
+      owner_user_id: {
+        type: Sequelize.BIGINT,
+      },
+      shared_to: {
+        type: Sequelize.ARRAY(Sequelize.BIGINT),
+      },
+      is_last_version: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        required: true,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('document');
+  },
+};
